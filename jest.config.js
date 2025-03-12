868f72d3bb6c0c0bc71a6735528a315c792842ee
Tamiris Cristine <tamiris.css@gmail.com>
2025-03-12 20:46:36 +0100

    chore: add Jest setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: [
+    "**/__tests__/**/*.test.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
+  },
+};
